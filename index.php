da55e2eb4f8e277c5965aea38beb2cfcddd774e4
byfirat <firat.kapar@gmail.com>
2022-04-28 12:38:49 +0300

    ddd

--- index.php
+++ index.php
@@ -68,13 +68,6 @@ if ( session_status() == PHP_SESSION_NONE ) {
 <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 
-<!-- Hızlı veri girişi kütüphaneleri -->
-<script src="jspreadsheet/jspreadsheet.js"></script>
-<script src="jspreadsheet/jsuites.js"></script>
-<link rel="stylesheet" href="jspreadsheet/jsuites.css" type="text/css" />
-<link rel="stylesheet" href="jspreadsheet/jspreadsheet.css" type="text/css" />
-<link rel="stylesheet" href="jspreadsheet/custom.css" type="text/css" />
-
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
@@ -137,6 +130,17 @@ if ( session_status() == PHP_SESSION_NONE ) {
 <!-- Toastr -->
 <script src="plugins/toastr/toastr.min.js"></script>
 <script src="_js/mesaj.js"></script>
+
+
+
+<!-- Hızlı veri girişi kütüphaneleri -->
+<script src="jspreadsheet/jspreadsheet.js"></script>
+<script src="jspreadsheet/jsuites.js"></script>
+<link rel="stylesheet" href="jspreadsheet/jsuites.css" type="text/css" />
+<link rel="stylesheet" href="jspreadsheet/jspreadsheet.css" type="text/css" />
+<link rel="stylesheet" href="jspreadsheet/custom.css" type="text/css" />
+
+
 
 </head>
 	<?php 
