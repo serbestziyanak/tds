a54d62d9850294bd3c5a30c9b007f083c9a823b7
Resul Evis <103247169+resulevis@users.noreply.github.com>
2023-01-03 14:46:11 +0300

    Puantaj

--- _modul/puantaj/puantaj.php
+++ _modul/puantaj/puantaj.php
@@ -10,7 +10,7 @@ if( array_key_exists( 'sonuclar', $_SESSION ) ) {
 	unset( $_SESSION[ 'sonuclar' ] );
 	echo "<script>mesajVer('$mesaj', '$mesaj_turu')</script>";
 }
-
+echo "asdas";
 $islem			= array_key_exists( 'islem'		,$_REQUEST ) 		? $_REQUEST[ 'islem' ]			: 'ekle';
 $personel_id	= array_key_exists( 'personel_id'	,$_REQUEST ) 	? $_REQUEST[ 'personel_id' ]	: 0;
 $detay			= array_key_exists( 'detay'		,$_REQUEST ) 		? $_REQUEST[ 'detay' ]			: null;
