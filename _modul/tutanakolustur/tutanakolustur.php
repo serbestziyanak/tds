98c9817abc8da8cc5568b499c7d07c546744203a
Resul Evis <103247169+resulevis@users.noreply.github.com>
2023-02-16 11:51:30 +0300

    Tutanak Oluşturma Savunma İstem  Yazısı

--- _modul/tutanakolustur/tutanakolustur.php
+++ _modul/tutanakolustur/tutanakolustur.php
@@ -9,7 +9,10 @@ $tarih			= array_key_exists( 'tarih'		    , $_REQUEST ) ? $_REQUEST[ 'tarih' ]		
 $SQL_tek_personel_oku = <<< SQL
 SELECT
 	*,
-	CONCAT(adi, ' ', soyadi) AS adsoyad
+	CONCAT(adi, ' ', soyadi) AS adsoyad,
+	(
+		SELECT tb_bolumler.adi FROM tb_bolumler WHERE tb_bolumler.id = tb_personel.bolum_id
+	) AS bolum_adi 
 FROM
 	tb_personel
 WHERE
@@ -85,14 +88,15 @@ $(document).ready(function() {
 	if ( tutanak_tipi 		== 'gunluk' ) {
 
 		tutanak_icerigi = '\u200B\t \u200B\t Yukarıda Unvanı adresi yazılı işyerimizde çalışan <?php echo $personel[0]["tc_no"]; ?> T.C. kimlik numaralı <?php echo $personel[0]["adsoyad"] ?> isimli işçimiz  <?php echo date("d.m.Y", strtotime($_REQUEST["tarih"])); ?> tarihinde iznimiz ve bilgimiz dışında mazeretsiz olarak mesaisine gelmemiştir.\n\n\u200B \t\u200B\t İşbu tutanak <?php echo date("d.m.Y") ?> tarihinde aşağıda isimleri yazılı şahitler huzurunda düzenlenmiş ve müştereken imza altına alınmıştır.';
-
+		var tip 			= 'Mazaretsiz Gelmeme ';
 	}else if ( tutanak_tipi == 'gecgelme' ) {
 
 		tutanak_icerigi = '\u200B\t \u200B\t Yukarıda Unvanı adresi yazılı işyerimizde çalışan <?php echo $personel[0]["tc_no"]; ?> T.C. kimlik numaralı <?php echo $personel[0]["adsoyad"] ?> isimli işçimiz <?php echo date("d.m.Y", strtotime($_REQUEST["tarih"])); ?> tarihinde iznimiz ve bilgimiz olmaksızın mazeretsiz olarak saat <?php echo $_REQUEST["saat"]."\'".$ek; ?> mesaisine gelmiştir.\n\n\u200B \t\u200B\t İşbu tutanak <?php echo date("d.m.Y") ?> tarihinde aşağıda isimleri yazılı şahitler huzurunda düzenlenmiş ve müştereken imza altına alınmıştır.';
-
+		var tip 			= 'Mazaretsiz  Geç Gelme';
 	}else if ( tutanak_tipi == 'erkencikma' ){
 
 		tutanak_icerigi = '\u200B\t \u200B\t Yukarıda Unvanı adresi yazılı işyerimizde çalışan <?php echo $personel[0]["tc_no"]; ?> T.C. kimlik numaralı <?php echo $personel[0]["adsoyad"] ?> isimli işçimiz <?php echo date("d.m.Y", strtotime($_REQUEST["tarih"])); ?> tarihinde iznimiz ve bilgimiz olmaksızın mazeretsiz olarak saat <?php echo $_REQUEST["saat"]."\'".$ek; ?> iş yerimizden ayrılmıştır.\n\n\u200B \t\u200B\t İşbu tutanak <?php echo date("d.m.Y") ?> tarihinde aşağıda isimleri yazılı şahitler huzurunda düzenlenmiş ve müştereken imza altına alınmıştır.';
+		var tip 			= 'Mazaretsiz Erken Çıkma';
 	}
 
 		var tarih = new Date();
@@ -136,7 +140,132 @@ $(document).ready(function() {
 					style:'firmaBilgisi'
 				},{
 					text: tutanak_icerigi,
+					style: 'metin',
+					pageBreak: 'after'
+				},
+				{
+					text: '\nSAVUNMA İSTEM YAZISI\n\n',
+					style: 'header',
+					alignment: 'center'
+				},
+				{
+					columns: [
+						{   
+							width: 160,
+							text: [
+								{text:'İŞVERENİN\n\n', style:'isveren'},
+								{text:'Ünvanı\n\n', style:'unvan'},
+								{text:'Adresi\n\n\n', style:'adres'},
+								{text:'SGK İşyeri Sicil Numarası', style:'sicil'}
+							],
+						},
+						{
+							width: 10,
+							text: [
+								{text:'\n\n', style:''},
+								{text:':\n\n', style:''},
+								{text:':\n\n\n', style:''},
+								{text:':', style:''}
+							],
+						},
+						{  
+						width: 'auto',
+						text: [
+								{text:'\n\n', style:''},
+								{text:'<?php echo $firma[ "firma" ]; ?>\n\n', style:''},
+								{text:'<?php echo $firma[ "adres" ];  ?>\n\n'},
+								{text:'<?php echo $firma[ "ticaret_sicil_no" ]?>', style:''}
+							],
+						}
+					],
+					style:'bosluk'
+				},
+				{
+					columns: [
+						{   
+							width: 160,
+							text: [
+								{text:'İŞÇİNİN\n\n', style:'isveren'},
+								{text:'Adı Soyadı\n', style:'unvan'},
+								{text:'T.C. Kimlik No\n', style:'adres'},
+								{text:'Bölümü\n\n', style:'sicil'},
+								{text:'Savunma İstem Tarihi', style:'adres'}
+							],
+						},
+						{
+							width: 10,
+							text: [
+								{text:'\n\n', style:''},
+								{text:':\n', style:''},
+								{text:':\n', style:''},
+								{text:':\n\n', style:''},
+								{text:':', style:''},
+							],
+						},
+						{  
+						width: 'auto',
+						text: [
+								{text:'\n\n', style:''},
+								{text:'<?php echo $personel[0]["adsoyad"] ?>\n', style:''},
+								{text:'<?php echo $personel[0]["tc_no"]; ?>\n'},
+								{text:'<?php echo $personel[0]["bolum_adi"]; ?>\n\n', style:''},
+								{text:'<?php echo date("d/m/Y"); ?>', style:''},
+							],
+						}
+					],
+					style:'bosluk'
+				},
+				{
+					text: '\u200B\t \u200B\t<?php echo date("d/m/Y", strtotime($_REQUEST["tarih"])); ?> tarihinde yapmamanız gerektiği halde yapmış olduğunuz  "'+tip+'" '+
+					' şeklindeki aykırı hareketinizle ilgili 4857 sayılı İş Kanununun 25/II/… Maddesi göz önünde' +
+					'bulundurularak, 109. maddesi uyarınca …../……/……tarihine kadar savunma yapmanız gerekmektedir.' +
+					'Savunmanızı haklı kılacak geçerli belgelerinizin olması halinde bu belgeleri savunmanıza eklemeniz, ' +
+					'aksi takdirde savunmanızın soyut kalacağı ve bahaneden öteye gitmeyeceğini, savunma yapmamanız' +
+					'halinde ise, aykırı davranışı kabul etmiş sayılacağınızı peşinen bildiririz.' +
+					'\n\n \u200B\t \u200B\tDüzenleyeceğiniz savunma yazınızı en son ..…/……/202… tarihinde saat 17:00 ‘a kadar İnsan ' +
+					'Kaynakları Departmanına vermeniz hususunu bilgilerinize sunarız.' +
+					'\n\nBir nüshasını aldım.\n\nTarih: ...../...../202...\n\n',
 					style: 'metin'
+				},
+				{
+					columns: [
+						{   
+							text: [
+								{text:'İşçinin Adı Soyadı\n',style:'imza'},
+								{text:'İmzası', style:'imza'}
+							],
+						},
+						{
+						},
+						{
+						},
+						{  
+						width: 'auto',
+						text: [
+								{text:'İşveren/İşveren Vekili\n',style:'imza'},
+								{text:'Adı Soyadı', style:'imza'}
+							],
+						}
+					]
+				},
+				{
+					text:[
+					{text:'\n\n\n\n( AŞAĞIDAKİ TUTANAK İŞÇİNİN SAVUNMA YAZISINI ALMAMASI HALİNE KULLANILACAKTIR. )\n\n\n'},
+					{text:'İşçi  işbu savunma istem yazısının içeriği hakkında bilgi almış (yazı kendisinin yüzene karşı okunmuş/yazıyı kendisi okumuş) fakat imzadan imtina etmiştir.\nTarih:…/…./202...'},  
+					],
+					
+				},{
+					columns: [
+						{   
+							text:'Tanık',style:'center'
+						},
+						{   
+							text:'Tanık',style:'center',
+						},
+						{   
+							text:'Tanık',style:'center',
+						},
+					]
 				},
 			],
 			styles: {
@@ -149,6 +278,11 @@ $(document).ready(function() {
 					marginBottom: 50,
 					alignment: 'left'
 				},
+				bosluk: {
+					marginBottom: 25,
+					alignment: 'left'
+				},
+
 				imza: {
 					marginTop: 100,
 					alignment: 'center'
@@ -159,16 +293,18 @@ $(document).ready(function() {
 				isveren:{
 				    decoration:'underline',
 				    bold:true
+				},
+				center:{
+					marginTop: 10,
+					alignment:'center'
 				}
-				
-				
 			},
 			info: 
 			{
 		    	title: 'Günlük Devamsızlık Tutanağı',
 		    	author: 'Syntax Yazılım PDKS',
 		    	creator:'Syntax Yazılım PDKS',
-		    	producer:'Syntax Yazılım PDKWS',
+		    	producer:'Syntax Yazılım PDKS',
 		    	subject:'Günlük Devamsızlık Tutanağı'
 		    }
 	    };
