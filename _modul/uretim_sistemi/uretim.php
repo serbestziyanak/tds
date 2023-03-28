84f518f959d874cbdc5176819f032e849cf5f8b3
byfirat <firat.kapar@gmail.com>
2023-03-28 22:32:33 +0300

    werwrwrwr

--- _modul/uretim_sistemi/uretim.php
+++ _modul/uretim_sistemi/uretim.php
@@ -42,39 +42,39 @@
 								<h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
 								<div class="timeline-body">
 <div class="card card-widget widget-user-2">
-<!-- Add the bg color to the header using any of the bg-* classes -->
-<div class="widget-user-header bg-warning">
-<div class="widget-user-image">
-<img class="img-circle elevation-2" src="../dist/img/user7-128x128.jpg" alt="User Avatar">
-</div>
-<!-- /.widget-user-image -->
-<h3 class="widget-user-username">Nadia Carmichael</h3>
-<h5 class="widget-user-desc">Lead Developer</h5>
-</div>
-<div class="card-footer p-0">
-<ul class="nav flex-column">
-<li class="nav-item">
-<a href="#" class="nav-link">
-Projects <span class="float-right badge bg-primary">31</span>
-</a>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link">
-Tasks <span class="float-right badge bg-info">5</span>
-</a>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link">
-Completed Projects <span class="float-right badge bg-success">12</span>
-</a>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link">
-Followers <span class="float-right badge bg-danger">842</span>
-</a>
-</li>
-</ul>
-</div>
+	<!-- Add the bg color to the header using any of the bg-* classes -->
+	<div class="widget-user-header bg-success">
+		<div class="widget-user-image">
+		<img class="img-circle elevation-2" src="../dist/img/user7-128x128.jpg" alt="User Avatar">
+		</div>
+		<!-- /.widget-user-image -->
+		<h3 class="widget-user-username">Fırat KAPAR</h3>
+		<h5 class="widget-user-desc">#133</h5>
+	</div>
+	<div class="card-footer p-0">
+		<ul class="nav flex-column">
+			<li class="nav-item">
+				<a href="#" class="nav-link">
+				İş <span class="float-right badge bg-primary">Ukrayna Kamuflaj Üretimi</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">
+				İstasyon <span class="float-right badge bg-info">Cep Dikme</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">
+					Hedef <span class="float-right badge bg-success">12</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">
+				Tamamlanan <span class="float-right badge bg-danger">842</span>
+				</a>
+			</li>
+		</ul>
+	</div>
 </div>
 								</div>
 							</div>
