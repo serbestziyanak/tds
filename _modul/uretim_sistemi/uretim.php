97067b41a7507e3978466a3eabe91b89dd95f8eb
byfirat <firat.kapar@gmail.com>
2023-03-28 22:39:09 +0300

    hhhh

--- _modul/uretim_sistemi/uretim.php
+++ _modul/uretim_sistemi/uretim.php
@@ -19,63 +19,44 @@
 						<div>
 							<i class="fa fa-camera bg-purple"></i>
 							<div class="timeline-item">
-								<span class="time"><i class="fas fa-clock"></i> 2 days ago</span>
-								<h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-								<div class="timeline-body">
-									<img src="https://placehold.it/150x100" alt="...">
-									<img src="https://placehold.it/150x100" alt="...">
-									<img src="https://placehold.it/150x100" alt="...">
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="timeline">
-						<!-- div class="time-label">
-							<span class="bg-green">3 Jan. 2014</span>
-						</div -->
-						<div>
-							<i class="fa fa-camera bg-purple"></i>
-							<div class="timeline-item">
 								<span class="time"><i class="fas fa-clock"></i> 28.03.2023</span>
 								<h3 class="timeline-header"><a href="#">Cep Dikimi</a> Zara Pantolon Üretimi</h3>
 								<div class="timeline-body">
-<div class="card card-widget widget-user-2">
-	<!-- Add the bg color to the header using any of the bg-* classes -->
-	<div class="widget-user-header bg-success">
-		<div class="widget-user-image">
-		<img class="img-circle elevation-2" src="../dist/img/user7-128x128.jpg" alt="User Avatar">
-		</div>
-		<!-- /.widget-user-image -->
-		<h3 class="widget-user-username">Fırat KAPAR</h3>
-		<h5 class="widget-user-desc">#133</h5>
-	</div>
-	<div class="card-footer p-0">
-		<ul class="nav flex-column">
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-				İş <span class="float-right badge bg-primary">Zara Pantolon Üretimi</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-				İstasyon <span class="float-right badge bg-info">Cep Dikme</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-					Hedef <span class="float-right badge bg-success">12</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-				Tamamlanan <span class="float-right badge bg-danger">842</span>
-				</a>
-			</li>
-		</ul>
-	</div>
-</div>
+									<div class="card card-widget widget-user-2">
+										<!-- Add the bg color to the header using any of the bg-* classes -->
+										<div class="widget-user-header bg-success">
+											<div class="widget-user-image">
+											<img class="img-circle elevation-2" src="../dist/img/user7-128x128.jpg" alt="User Avatar">
+											</div>
+											<!-- /.widget-user-image -->
+											<h3 class="widget-user-username">Fırat KAPAR</h3>
+											<h5 class="widget-user-desc">#133</h5>
+										</div>
+										<div class="card-footer p-0">
+											<ul class="nav flex-column">
+												<li class="nav-item">
+													<a href="#" class="nav-link">
+													İş <span class="float-right badge bg-primary">Zara Pantolon Üretimi</span>
+													</a>
+												</li>
+												<li class="nav-item">
+													<a href="#" class="nav-link">
+													İstasyon <span class="float-right badge bg-info">Cep Dikme</span>
+													</a>
+												</li>
+												<li class="nav-item">
+													<a href="#" class="nav-link">
+														Hedef <span class="float-right badge bg-success"><h3>12</h3></span>
+													</a>
+												</li>
+												<li class="nav-item">
+													<a href="#" class="nav-link">
+													Tamamlanan <span class="float-right badge bg-danger"><h3>842</h3></span>
+													</a>
+												</li>
+											</ul>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
