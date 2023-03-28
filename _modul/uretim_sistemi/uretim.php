6b3fe5260c3aca7410cf5f17721e2245e1ee8c86
byfirat <firat.kapar@gmail.com>
2023-03-29 00:13:21 +0300

    istasyon açma kapama

--- _modul/uretim_sistemi/uretim.php
+++ _modul/uretim_sistemi/uretim.php
@@ -15,13 +15,25 @@
  letter-spacing: .2rem;
 }
 </style>
+
+<script>
+
+function istasyonGoster(id){
+	$(id).show(200);
+}
+
+function istasyonGizle(id){
+	$(id).hide(200);
+}
+
+</script>
 <div class="content-wrapper">
 	<section class="content-header">
 		
 		<div class="container-fluid">
 			<div class="row mb-2">
-				<div class="col-sm-6">
-					<h1>Üretim Hattı</h1>
+				<div class="col-sm-12">
+					<center><h1><b>Üretim Hattı</b></h1></center>
 				</div>
 			</div>
 		</div>
@@ -32,13 +44,13 @@
 				<div class="col-md-4">
 					<div class="timeline">
 						<div class="time-label">
-							<span class="bg-primary"><a href = "#">  <img class="img-circle elevation-2" src="resimler/resim_yok.jpg" width = "40px" alt="User Avatar"> Fırat KAPAR</a></span>
+							<span class="bg-secondary"><a href = "#" onclick = "istasyonGoster('#istasyon_1')">  <img class="img-circle elevation-2" src="resimler/resim_yok.jpg" width = "40px" alt="User Avatar"> Fırat KAPAR</a></span>
 						</div>
 						<div>
-							<div class="timeline-item">
+							<div class="timeline-item" id = "istasyon_1" style = "display:none;">
 								<span class="time">
 									<i class="fas fa-clock"></i> 28.03.2023
-									<button type="button" class="btn btn-tool" timeline-item="remove"><i class="fas fa-times fa-lg"></i></button>
+									<button type="button" class="btn btn-tool"><i class="fas fa-times fa-lg" onclick = "istasyonGizle('#istasyon_1')"></i></button>
 								</span>
 								<h3 class="timeline-header"><a href="#">Cep Dikimi</a> Zara Pantolon Üretimi</h3>
 								<div class="timeline-body">
@@ -71,6 +83,53 @@
 								</div>
 							</div>
 						</div>
+		
+		
+						<div class="time-label">
+							<span class="bg-success"><a href = "#" onclick = "istasyonGoster('#istasyon_2')">  <img class="img-circle elevation-2" src="resimler/resim_yok.jpg" width = "40px" alt="User Avatar"> Serbest Ziyanak</a></span>
+						</div>
+						<div>
+							<div class="timeline-item" id = "istasyon_2" style = "display:none;">
+								<span class="time">
+									<i class="fas fa-clock"></i> 28.03.2023
+									<button type="button" class="btn btn-tool"><i class="fas fa-times fa-lg" onclick = "istasyonGizle('#istasyon_2')"></i></button>
+								</span>
+								<h3 class="timeline-header"><a href="#">Cep Dikimi</a> Zara Pantolon Üretimi</h3>
+								<div class="timeline-body">
+									<div class="card card-widget widget-user-2">
+										<!-- Add the bg color to the header using any of the bg-* classes -->
+										<div class="widget-user-header bg-success">
+											<div class="widget-user-image">
+											<img class="img-circle elevation-2" src="resimler/resim_yok.jpg" alt="User Avatar">
+											</div>
+											<!-- /.widget-user-image -->
+											<h3 class="widget-user-username">Fırat KAPAR</h3>
+											<h5 class="widget-user-desc">#133</h5>
+										</div>
+										<div class="card-footer p-0">
+											<ul class="nav flex-column">
+	
+												<li class="nav-item">
+													<a href="#" class="nav-link">
+													Hedef(Günlük) <span class="float-right badge bg-primary badge-number">842</span>
+													</a>
+												</li>
+												<li class="nav-item">
+													<a href="#" class="nav-link">
+													Tamamlanan <span class="float-right badge bg-danger badge-number">250</span>
+													</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+		
+		
+		
+		
+		
 					</div>
 				</div>
 			</div>
