ffb8a5af50089c3e8afa3b8050a5cf54d50f0caf
byfirat <firat.kapar@gmail.com>
2023-03-28 22:20:59 +0300

    sss

--- _modul/uretim_sistemi/uretim.php
+++ _modul/uretim_sistemi/uretim.php
@@ -2,9 +2,9 @@
 	<section class="content-header">
 		<div class="container-fluid">
 			<div class="row mb-2">
-			<div class="col-sm-6">
-				<h1>Üretim Hattı</h1>
-			</div>
+				<div class="col-sm-6">
+					<h1>Üretim Hattı</h1>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -13,23 +13,39 @@
 			<div class="row">
 				<div class="col-md-4">
 					<div class="timeline">
-					<!-- div class="time-label">
-						<span class="bg-green">3 Jan. 2014</span>
-					</div -->
-					<div>
-						<i class="fa fa-camera bg-purple"></i>
-						<div class="timeline-item">
-							<span class="time"><i class="fas fa-clock"></i> 2 days ago</span>
-							<h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-							<div class="timeline-body">
-								<img src="https://placehold.it/150x100" alt="...">
-								<img src="https://placehold.it/150x100" alt="...">
-								<img src="https://placehold.it/150x100" alt="...">
-								<img src="https://placehold.it/150x100" alt="...">
-								<img src="https://placehold.it/150x100" alt="...">
+						<!-- div class="time-label">
+							<span class="bg-green">3 Jan. 2014</span>
+						</div -->
+						<div>
+							<i class="fa fa-camera bg-purple"></i>
+							<div class="timeline-item">
+								<span class="time"><i class="fas fa-clock"></i> 2 days ago</span>
+								<h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+								<div class="timeline-body">
+									<img src="https://placehold.it/150x100" alt="...">
+									<img src="https://placehold.it/150x100" alt="...">
+									<img src="https://placehold.it/150x100" alt="...">
+								</div>
 							</div>
 						</div>
 					</div>
+				</div>
+					<div class="timeline">
+						<!-- div class="time-label">
+							<span class="bg-green">3 Jan. 2014</span>
+						</div -->
+						<div>
+							<i class="fa fa-camera bg-purple"></i>
+							<div class="timeline-item">
+								<span class="time"><i class="fas fa-clock"></i> 2 days ago</span>
+								<h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+								<div class="timeline-body">
+									<img src="https://placehold.it/150x100" alt="...">
+									<img src="https://placehold.it/150x100" alt="...">
+									<img src="https://placehold.it/150x100" alt="...">
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
