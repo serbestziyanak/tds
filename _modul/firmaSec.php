0ba4cc4feb0f2e4517b0f06b8e0bdf6aaea28ce5
Resul Evis <103247169+resulevis@users.noreply.github.com>
2023-07-11 13:58:54 +0300

    asdasd

--- _modul/firmaSec.php
+++ _modul/firmaSec.php
@@ -23,8 +23,8 @@ if (in_array(  $_REQUEST["firma_id"], $firmaListesi)) {
 	$_SESSION[ 'firma_adi' ]		= $_REQUEST["firma_adi"];
 
 	/*Son işlem Yaptığı Firmayı Cerezde Tutma*/
-	setcookie('firma_id', 		$_REQUEST["firma_id"], $expire,"/","",false,false);
-	setcookie('firma_adi', 		$_REQUEST["firma_adi"], $expire,"/","",false,false);
+	setcookie('firma_id', 		$_REQUEST["firma_id"], $expire,"/","",true,true);
+	setcookie('firma_adi', 		$_REQUEST["firma_adi"], $expire,"/","",true,true);
 
 	header( "Location: ../index.php" );
 }?> 
